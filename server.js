ed3819711931179461c6717e29b57c759b2f0220
stefanparaschivescu <stefparaschivescu@gmail.com>
2020-10-09 18:59:18 +0300

    fixed bugs with packages dependecies and server port

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/project-name'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/project-name' }
+  );
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is running on port ${PORT}.`);
+}); 
+
